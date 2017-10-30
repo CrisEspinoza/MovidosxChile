353ccad0a724d3a01cd4053eb258aff87a56e1f5
JulioSerrano <julio.serrano@usach.cl>
2017-10-30 19:28:37 -0300

    push para seguir en windows

---
+++ database/migrations/2017_10_30_180614_create_historials_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistorialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('Fecha');
+            
+            //fk
+            $table->integer('Id_Usuario')->unsigned()->nullable();
+            $table->foreign('Id_Usuario')->references('id')->on('users');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('historials');
+        Schema::enableForeignKeyConstraints();
+    }
+}
