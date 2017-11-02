d9632716fd308422f67287cee7450932e1f900ff
TomasGutierrezL <tomas.gutierrez.l@usach.cl>
2017-11-02 23:57:37 +0000

    Llenado de tablas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -34,9 +34,6 @@ class DatabaseSeeder extends Seeder
           'Tipo_Rol' => 1,
         ]);
 
-
-
-
         DB::table('usuarios')->insert([
           'id' => 1,
           'id_rol' => 1,
@@ -47,14 +44,71 @@ class DatabaseSeeder extends Seeder
           'Mail'=> 'julio.serrano@usach.cl'
         ]);
 
+        DB::table('rols')->insert([
+          'id' => 2,
+          'Tipo_Rol' => 1,
+        ]);
+
+        DB::table('usuarios')->insert([
+          'id' => 2,
+          'id_rol' => 1,
+          'Rut'=> '16.509.634-k',
+          'Nombres'=> 'Claudio Hector',
+          'Apellidos'=> 'Alvarez Espinosa',
+          'password' =>'secret',
+          'Mail'=> 'claudio.alvarez@gmail.com'
+        ]);
 
 
+        DB::table('rols')->insert([
+          'id' => 3,
+          'Tipo_Rol' => 1,
+        ]);
+
+        DB::table('usuarios')->insert([
+          'id' => 3,
+          'id_rol' => 1,
+          'Rut'=> '18.809.352-6',
+          'Nombres'=> 'Miguel Angel',
+          'Apellidos'=> 'Caceres Cabello',
+          'password' =>'secret',
+          'Mail'=> 'miguel.angel@gmail.com'
+        ]);
+
+        DB::table('rols')->insert([
+          'id' => 4,
+          'Tipo_Rol' => 1,
+        ]);
+
+        DB::table('usuarios')->insert([
+          'id' => 4,
+          'id_rol' => 1,
+          'Rut'=> '19.321.433-1',
+          'Nombres'=> 'Jorge Eleuterio',
+          'Apellidos'=> 'Godoy Rojas',
+          'password' =>'secret',
+          'Mail'=> 'jorge.rojas@usach.cl'
+        ]);
 
         DB::table('bancos')->insert([
           'id' => 1,
           'Nombre_Banco' => 'banco santander',
         ]);
 
+        DB::table('bancos')->insert([
+          'id' => 2,
+          'Nombre_Banco' => 'banco bci',
+        ]);
+
+        DB::table('bancos')->insert([
+          'id' => 3,
+          'Nombre_Banco' => 'banco de chile',
+        ]);
+
+        DB::table('bancos')->insert([
+          'id' => 4,
+          'Nombre_Banco' => 'banco falabella',
+        ]);
 
 
         DB::table('biens')->insert([
@@ -63,8 +117,11 @@ class DatabaseSeeder extends Seeder
           'Nombre_Bien' => 'Ropa',
         ]);
 
-
-
+        DB::table('biens')->insert([
+          'id' => 2,
+          'Tipo_Bien' => 'pantalon',
+          'Nombre_Bien' => 'Ropa',
+        ]);
 
         DB::table('ubicacions')->insert([
           'id' => 1,
@@ -73,7 +130,33 @@ class DatabaseSeeder extends Seeder
           'Ciudad' => 'Santiago',
         ]);
 
+        DB::table('ubicacions')->insert([
+          'id' => 2,
+          'Calle' => 'Presidente Carlos Davila',
+          'Comuna' => 'Maipu',
+          'Ciudad' => 'Santiago',
+        ]);
 
+        DB::table('ubicacions')->insert([
+          'id' => 3,
+          'Calle' => 'Luis Beltran',
+          'Comuna' => 'Pudahuel',
+          'Ciudad' => 'Santiago',
+        ]);
+
+        DB::table('ubicacions')->insert([
+          'id' => 4,
+          'Calle' => 'Avenida las torres',
+          'Comuna' => 'Quilicura',
+          'Ciudad' => 'Santiago',
+        ]);
+
+        DB::table('ubicacions')->insert([
+          'id' => 5,
+          'Calle' => 'Las bellotas',
+          'Comuna' => 'Concon',
+          'Ciudad' => 'Valparaiso',
+        ]);
 
 
         DB::table('permisos')->insert([
@@ -112,8 +195,26 @@ class DatabaseSeeder extends Seeder
           'Edad' => 20,
         ]);
 
+        DB::table('voluntarios')->insert([
+          'id' => 2,
+          'Nombres_Voluntario' => 'Jorge Andres',
+          'Apellidos_Voluntario' => 'Lopez Castro',
+          'Edad' => 32,
+        ]);
 
+        DB::table('voluntarios')->insert([
+          'id' => 3,
+          'Nombres_Voluntario' => 'Rosa Ester',
+          'Apellidos_Voluntario' => 'Navarrete Pizarro',
+          'Edad' => 26,
+        ]);
 
+        DB::table('voluntarios')->insert([
+          'id' => 4,
+          'Nombres_Voluntario' => 'Paula Sofia',
+          'Apellidos_Voluntario' => 'Poblete Illesca',
+          'Edad' => 24,
+        ]);
 
 
 
@@ -182,6 +283,21 @@ class DatabaseSeeder extends Seeder
           'Cantidad_Maxima_Voluntarios' => 30,
         ]);
 
+        DB::table('voluntariados')->insert([
+          'id' => 2,
+          'Tipo_Trabajo' => 'Acopio de ropa',
+          'Perfil_Voluntario' => 'Disposicion y tiempo',
+          'Cantidad_Minima_Voluntarios' => 5,
+          'Cantidad_Maxima_Voluntarios' => 20,
+        ]);
+
+        DB::table('voluntariados')->insert([
+          'id' => 3,
+          'Tipo_Trabajo' => 'Cocina',
+          'Perfil_Voluntario' => 'Conocimientos basicos de la cocina',
+          'Cantidad_Minima_Voluntarios' => 2,
+          'Cantidad_Maxima_Voluntarios' => 5,
+        ]);
 
 
 
@@ -193,24 +309,36 @@ class DatabaseSeeder extends Seeder
           'id_banco' => 1,
         ]);
 
+        DB::table('donacions')->insert([
+          'id' => 2,
+          'Monto' => 2000,
+          'Tipo_Donacion' => 1 ,
+          'id_banco' => 3,
+        ]);
 
+        DB::table('donacions')->insert([
+          'id' => 3,
+          'Monto' => 1000,
+          'Tipo_Donacion' => 1 ,
+          'id_banco' => 3,
+        ]);
 
-
+        DB::table('donacions')->insert([
+          'id' => 4,
+          'Monto' => 15000,
+          'Tipo_Donacion' => 1 ,
+          'id_banco' => 2,
+        ]);
 
         DB::table('centro_acopio_bien')->insert([
           'id_bien' => 1 ,
-          'id_centroAcopio' => 1,
+          'id_centroAcopio' => 3,
         ]);
 
-
-
-
-
-
-
-
-
-
+        DB::table('centro_acopio_bien')->insert([
+          'id_bien' => 1 ,
+          'id_centroAcopio' => 2,
+        ]);
 
 
     }
