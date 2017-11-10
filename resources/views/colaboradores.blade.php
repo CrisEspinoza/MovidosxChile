0e5afbb6b57611cf34aacc9a71ecbf3545cacaac
TomasGutierrezL <tomas.gutierrez.l@usach.cl>
2017-11-10 16:03:51 -0300

    Fix colaboradores

--- resources/views/colaboradores.blade.php
+++ resources/views/colaboradores.blade.php
@@ -3,11 +3,7 @@
 @section('content')
 
 <div class="container">
-<<<<<<< HEAD
   <div class="jumbotron">
-=======
-  <div class="jumbotron"
->>>>>>> 9903b505cce4b3595fa7c07f013f99c6f83ca74a
   <h2>Lista de colaboradores</h2>
   <p> YO </p>
 </div>
