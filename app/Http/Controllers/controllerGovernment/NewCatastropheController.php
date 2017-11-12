89d6f602b35147d35ad0a473b40f933d65b0d16f
vagrant <vagrant@homestead>
2017-11-12 23:51:01 +0000

    vista de catastrofes del gobiernos casi completa

--- app/Http/Controllers/controllerGovernment/NewCatastropheController.php
+++ app/Http/Controllers/controllerGovernment/NewCatastropheController.php
@@ -4,81 +4,44 @@ namespace App\Http\Controllers\controllerGovernment;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Region;
+use App\Commune;
+use App\TypeCatastrophe;
 
 class NewCatastropheController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('/government/newCatastrophe'); 
+        $regions = Region::all()->sortby('id');
+        $communes = Commune::all()->sortby('region_id');
+        $typesCats = TypeCatastrophe::all()->sortby('id');
+        return view('/government/newCatastrophe', compact('regions', 'communes','typesCats')); 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        $validator = Validator::make($request->all(),
+            [
+            'name' => 'required|string|max:20',
+            'type' => 'required|integer',
+            'comunne_id' => 'required|integer',
+            'description' => 'required|string|min:5|max:255'
+            ],
+        [
+            'required' => 'Este campo es requerido',
+            'string' => 'Debe usar caracteres',
+            'max' => 'Cantidad mayor a la permitida',
+            'integer' => 'Debe ser un valor numérico'
+        ]
+            );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
     {
         //
     }
