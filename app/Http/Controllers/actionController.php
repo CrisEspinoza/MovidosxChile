91d2ea9c3b582390e90236fdc5867ad9abb180da
JulioSerrano <julio.serrano@usach.cl>
2017-11-27 16:20:51 -0300

    problem with middleware

--- app/Http/Controllers/actionController.php
+++ app/Http/Controllers/actionController.php
@@ -26,7 +26,7 @@ class actionController extends Controller
 
      public function __construct()
      {
-      $this->middleware('auth');
+       //$this->middleware('auth');
      }
 
     public function index($id)
